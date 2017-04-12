a110e82f9f35d5c891cdb36a5f3a48f16a95fab5
Jeremy Carroll <jjc@syapse.com>
2017-04-12 13:05:36 -0700

    Corrected jar name

--- go.sh
+++ go.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-JAR=/home/ubuntu/jjc/database/blazegraph-jar/target/blazegraph-jar-2.1.4-HEAD-SNAPSHOT.jar
+JAR=/home/ubuntu/jjc/database/blazegraph-jar/target/blazegraph-jar-debug_off_2_0_1-SNAPSHOT.jar
 /usr/lib/jvm/java-8-oracle/bin/java \
    -Djetty.start.timeout=600 -Dorg.eclipse.jetty.server.Request.maxFormContentSize=2000000000 \
    -XX:+UseG1GC -XX:+UseStringDeduplication -Xms2064800k -Xmx21329600k \
